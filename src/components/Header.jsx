d0832180c38bf5e4ecf5a8a40537581e3ea22f96
Jonathan Samines <jn.samines@gmail.com>
2016-11-06 21:32:58 -0600

    Improve applicatino styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,14 +28,21 @@ class Header extends React.Component {
         <nav>
           <ul className="nav nav-pills pull-right">
             <li role="presentation" className={this.state.isMc}>
-              <a href="#" onClick={this.setActiveMenu('mc')}>Costo Minimo</a>
+              <a href="#" onClick={this.setActiveMenu('mc')}>
+                Costo Minimo
+                </a>
             </li>
             <li role="presentation" className={this.state.isTransbord}>
-              <a href="#" onClick={this.setActiveMenu('trans')}>Metodo de Transbordos</a>
+              <a href="#" onClick={this.setActiveMenu('trans')}>
+                Metodo de Transbordos
+              </a>
             </li>
           </ul>
         </nav>
-        <h3 className="text-muted">Metodos de Transporte</h3>
+        <h3 className="text-muted">
+          <div className="logo"></div>
+          <span className="logo-title">Metodos de Transporte</span>
+        </h3>
       </div>
     );
   }
